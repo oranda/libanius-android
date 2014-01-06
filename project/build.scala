13be3d9579c84af04c4ed612f9d2adc47af1f1e0
James McCabe <james@oranda.com>
2014-01-06 04:02:51 +0000

    build 0.931

--- project/build.scala
+++ project/build.scala
@@ -7,8 +7,8 @@ object General {
 
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Libanius",
-    version := "0.93",
-    versionCode := 930,
+    version := "0.931",
+    versionCode := 931,
     scalaVersion := "2.10.2",
     platformName in Android := "android-14",  // formerly android-8
     scalacOptions += "-deprecation",
